c5bf966c0f12692ed5ff785d6f2d2f3ef7af17dc
sarun0929891578 <sarun0929891578@gmail.com>
2020-08-30 12:34:10 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export default {
+  products : [
+      { id: '1001', 
+      name: 'เสื้อเชิร์ต', 
+      pire: 200, 
+      stock: 10,
+      pic: './images/pic1 .jpg' },
+
+      { id: '1002', 
+      name: 'กางเกงยีนต์', 
+      pire: 1200, 
+      stock: 50,
+      pic: './images/pic2 .jpg' },
+
+      { id: '1003', 
+      name: 'รองเท้า', 
+      pire: 500, 
+      stock: 100,
+      pic: './images/pic3 .jpg' }
+  ]
+}
